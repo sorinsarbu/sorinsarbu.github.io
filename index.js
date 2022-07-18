a5973fffd96f37930419854b584dd9a2b2cc9327
Sorin Sarbu <simisarbu@yahoo.com>
2022-07-18 19:24:50 +0300

    make sure that the functions js are working fine

--- index.js
+++ index.js
@@ -12,20 +12,30 @@ age = 31;
 // console.info("My name is " + myName = ", I'm " + age + " years old.");
 
 function showHomePage() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
   document.getElementById("home").style.display = "block";
 }
 
 function showSkillsPage() {
+  console.info;
   document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
   document.getElementById("skills").style.display = "block";
 }
 
 function showProjectsPage() {
+  document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
   document.getElementById("projects").style.display = "block";
 }
 
 function showLanguagesPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
   document.getElementById("projects").style.display = "none";
   document.getElementById("languages").style.display = "block";
 }
