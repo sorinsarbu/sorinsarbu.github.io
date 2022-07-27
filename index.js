27d4afc0174df01b00a90369f561fb8e962e5e8f
Sorin Sarbu <simisarbu@yahoo.com>
2022-07-27 10:12:06 +0300

    new id (short) added on header-info

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ function showPage(nextPage) {
   console.warn("change", activePage, "to", nextPage);
   hide(activePage);
   show(nextPage);
-  document.getElementById("menu-" + activePage).classList.remove("active");
-  document.getElementById("menu-" + nextPage).classList.add("active");
+  document
+    .querySelector(`a[data-page=${activePage}]`)
+    .classList.remove("active");
+  document.querySelector(`a[data-page=${nextPage}]`).classList.add("active");
   activePage = nextPage;
 }
 
@@ -37,8 +39,7 @@ function initEvents() {
     .getElementById("top-menu-bar")
     .addEventListener("click", function (e) {
       if (e.target.matches("a")) {
-        var id = e.target.id.substring(5);
-        console.warn("click pe menu", id, e.target.matches("a"));
+        var id = e.target.getAttribute("data-page");
         showPage(id);
       }
     });
